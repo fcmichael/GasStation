9e09216c7b9185977eb5e9e6d873158720344f61
fcmichael <michael.1@wp.pl>
2018-01-20 12:38:40 +0100

    Init and add properties parser

---
+++ src/pl/michalkruk/SimGeneratorFactory.java
@@ -0,0 +1,73 @@
+package pl.michalkruk;
+
+import dissimlab.random.SimGenerator;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class SimGeneratorFactory {
+
+    private static final SimGenerator generator = new SimGenerator();
+    private static final String DELIMETER = ",";
+
+    public static double get(String expression) {
+        try {
+            Class c = Class.forName("dissimlab.random.SimGenerator");
+            Method m = c.getMethod(parseName(expression), parseParemeterTypes(expression));
+            return (double) m.invoke(generator, parseArguments(expression));
+        } catch (ClassNotFoundException | NoSuchMethodException | SecurityException | IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+
+    private static Object[] parseArguments(String expression) {
+        StringTokenizer tokenizer = new StringTokenizer(expression, DELIMETER);
+        tokenizer.nextToken();
+        List<Object> classes = new LinkedList<>();
+        while (tokenizer.hasMoreTokens()) {
+            String token = tokenizer.nextToken();
+            if (token.contains(".")) {
+                classes.add(Double.parseDouble(token));
+            } else {
+                classes.add(Integer.parseInt(token));
+            }
+        }
+        Object[] classTable = new Object[classes.size()];
+        for (int i = 0; i < classes.size(); i++) {
+            classTable[i] = classes.get(i);
+        }
+
+        return classTable;
+    }
+
+    private static Class[] parseParemeterTypes(String expression) {
+        StringTokenizer tokenizer = new StringTokenizer(expression, DELIMETER);
+        tokenizer.nextToken();
+        List<Class> classes = new LinkedList<>();
+        while (tokenizer.hasMoreTokens()) {
+            String token = tokenizer.nextToken();
+            if (token.contains(".")) {
+                classes.add(double.class);
+            } else {
+                classes.add(int.class);
+            }
+        }
+        Class[] classTable = new Class[classes.size()];
+        for (int i = 0; i < classes.size(); i++) {
+            classTable[i] = classes.get(i);
+        }
+
+        return classTable;
+
+    }
+
+    private static String parseName(String expression) {
+        StringTokenizer tokenizer = new StringTokenizer(expression, DELIMETER);
+        return tokenizer.nextToken();
+    }
+}
---
+++ src/pl/michalkruk/SimProperties.java
@@ -0,0 +1,63 @@
+package pl.michalkruk;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class SimProperties {
+
+    private static SimProperties instance = null;
+    private Properties properties;
+
+    private SimProperties() {
+        properties = new Properties();
+        loadPropertiesFile();
+    }
+
+    private void loadPropertiesFile() {
+        InputStream input = null;
+
+        try {
+            input = new FileInputStream("resources/application.properties");
+            properties.load(input);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } finally {
+            if (input != null) {
+                try {
+                    input.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public static SimProperties getInstance() {
+        if (instance == null) {
+            instance = new SimProperties();
+        }
+        return instance;
+    }
+
+
+    public int intFromProperties(String propName, int defaultValue) {
+        int ret = defaultValue;
+        String intString = properties.getProperty(propName);
+
+        if (intString != null) {
+            try {
+                ret = Integer.parseInt(intString.trim());
+            } catch (NumberFormatException e) {
+                ret = defaultValue;
+            }
+        }
+        return ret;
+    }
+
+    public String get(String propName) {
+        return properties.getProperty(propName);
+    }
+
+}
